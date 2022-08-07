7c5fa91923c2328d669d8c538b02746c8bd1e176
dmitriyzheleznyak <komenus@gmail.com>
2022-08-08 01:16:13 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB) 
+{
+    Console.WriteLine("Наибольшее число ");
+    Console.WriteLine(numberA);
+    Console.WriteLine("Наименьшее число ");
+    Console.WriteLine(numberB);
+    }
+else
+{
+   Console.WriteLine("Наибольшее число ");
+    Console.WriteLine(numberB);
+    Console.WriteLine("Наименьшее число ");
+    Console.WriteLine(numberA);
+}
