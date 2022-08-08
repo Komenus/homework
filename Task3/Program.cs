c52ade6f946a008c3005fa9313f4d16ea0a8229f
dmitriyzheleznyak <komenus@gmail.com>
2022-08-08 11:40:44 +0300

    Task3 even number

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA%2 == 0)
+{
+        Console.WriteLine("Число четное ");
+}
+else
+{
+    Console.WriteLine("Число нечетное ");
+}
