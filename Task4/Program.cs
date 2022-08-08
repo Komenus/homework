63de2f637418408f0f8cd6a611942297835841da
dmitriyzheleznyak <komenus@gmail.com>
2022-08-08 21:42:05 +0300

    task4_even

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа  ");
+int numberB = 0;
+while(numberB <= numberA)
+{
+    if (numberB%2 == 0)
+    {
+        Console.WriteLine(numberB);
+        numberB++;
+    }
+        else
+        numberB++;
+    {
+        }
+}        
