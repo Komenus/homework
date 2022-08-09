135e3a51ce332d2126fdcb5ec06ea235fd5d5bbe
dmitriyzheleznyak <komenus@gmail.com>
2022-08-09 20:16:18 +0300

    Task 4 change

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите число ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Четные числа  ");
-int numberB = 0;
+int numberB = 1;
 while(numberB <= numberA)
 {
     if (numberB%2 == 0)
